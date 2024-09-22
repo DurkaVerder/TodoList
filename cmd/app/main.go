3c3470346dd1e7d3f1d6b100324edf57288d06dc
DurlaVerder <vrrrr227@gmail.com>
2024-09-22 22:04:15 +0300

    Add Init db func

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"TodoList/internal/db"
+	"log"
+)
 
+func main() {
+	var err error
+	// Init Config
+
+	// Init DataBase
+	if err = db.InitDb(); err != nil {
+		log.Fatal("Error init database:", err)
+	}
+
+	// Init Handlers
+
+	// Run App
 }
---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+type Config struct {
+	DB struct {
+
+	}
+	Sv struct {
+		
+	}
+}
+
+func InitConfig() {
+
+}
