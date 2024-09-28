7e4b55d23b0e3201f109d5c67713e0263c078074
DurlaVerder <vrrrr227@gmail.com>
2024-09-28 23:10:07 +0300

    Make prototype func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"TodoList/internal/controller"
+	"TodoList/internal/database"
+	"TodoList/internal/repository"
+	"TodoList/internal/router"
+	"TodoList/internal/service"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	controller := &controller.TaskController{&service.TaskService{&repository.TaskRepository{database.Init()}}}
+
+	router.InitRouter(e, controller)
+
+	e.Start(":2222")
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"TodoList/internal/controller"
+
+	"github.com/labstack/echo"
+)
+
+func InitRouter(e *echo.Echo, taskController *controller.TaskController) {
+	e.GET("/task/:id", taskController.GetTask)
+	e.GET("/tasks", taskController.GetAllTasks)
+	e.POST("/add", taskController.AddTask)
+	e.PUT("/change/:id", taskController.ChangeTask)
+	e.DELETE("/delete/:id", taskController.DeleteTask)
+}
