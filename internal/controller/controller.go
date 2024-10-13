238679a2c2fadbec0beb5ca721e1c566d7d6d48c
DurlaVerder <vrrrr227@gmail.com>
2024-10-13 14:15:29 +0300

    Split interface in controller

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -2,21 +2,11 @@ package controller
 
 import (
 	"TodoList/internal/service"
-
-	"github.com/labstack/echo"
 )
 
 type Controller interface {
-	HandleLogin(ctx echo.Context) error
-	HandleRegister(ctx echo.Context) error
-	HandleGetUser(ctx echo.Context) error
-	HandleUpdateUser(ctx echo.Context) error
-	HandleDeleteUser(ctx echo.Context) error
-	HandleGetAllTasks(ctx echo.Context) error
-	HandleGetTask(ctx echo.Context) error
-	HandleCreateTask(ctx echo.Context) error
-	HandleUpdateTask(ctx echo.Context) error
-	HandleDeleteTask(ctx echo.Context) error
+	UserController
+	TaskController
 }
 
 type ControllerManager struct {
--- internal/controller/user.go
+++ internal/controller/user.go
@@ -2,6 +2,14 @@ package controller
 
 import "github.com/labstack/echo"
 
+type UserController interface {
+	HandleLogin(ctx echo.Context) error
+	HandleRegister(ctx echo.Context) error
+	HandleGetUser(ctx echo.Context) error
+	HandleUpdateUser(ctx echo.Context) error
+	HandleDeleteUser(ctx echo.Context) error
+}
+
 func (c *ControllerManager) HandleLogin(ctx echo.Context) error {
 	return nil
 }
